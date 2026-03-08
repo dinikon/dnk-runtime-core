5d22e70088f4e7c7014f8ce5ccc1cfab8dd7e3b8
dNikon <denis.inikon@gmail.com>
2026-03-08 11:49:05 +0200

    feat(shared): add `TenantBase` with tenant schema support
    - Introduced `TenantBase` class to enable automatic tenant schema handling using `TENANT_SCHEMA_ALIAS`.

--- src/modules/shared/db/base.py
+++ src/modules/shared/db/base.py
@@ -2,10 +2,15 @@ from sqlalchemy import JSON, MetaData
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import DeclarativeBase
 
+TENANT_SCHEMA_ALIAS = "tenant"
 
 class Base(DeclarativeBase):
     __abstract__ = True
     metadata = MetaData()
+
+class TenantBase(DeclarativeBase):
+    __abstract__ = True
+    metadata = MetaData(schema=TENANT_SCHEMA_ALIAS)
 
 
 PortableJSON = JSON().with_variant(JSONB(), "postgresql")
