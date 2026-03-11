ccbcbfdfed7aa5e0c82fb7520de22ac2b123ddd9
dNikon <denis.inikon@gmail.com>
2026-03-11 22:50:48 +0200

    chore(runtime_schema): extend `DataSource` entity with additional attributes
    - Added `created_at`, `updated_at`, `tenant_id`, `type`, `schema`, `is_system`, `is_remote`, and `dsn` fields to `DataSource` entity.
    - Enhanced support for metadata and configuration within the runtime schema domain.

--- src/modules/runtime_schema/domain/source/entity.py
+++ src/modules/runtime_schema/domain/source/entity.py
@@ -1,8 +1,19 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 from modules.runtime_schema.domain.source.value_object import DataSourceIdVO
+from modules.shared.domain.value_object.entity_id import EntityIdVO
 
 
 @dataclass(slots=True)
 class DataSource:
     id: DataSourceIdVO
+    created_at: datetime
+    updated_at: datetime
+    tenant_id: EntityIdVO
+
+    type: str
+    schema: str
+    is_system: bool
+    is_remote: bool
+    dsn: str | None
